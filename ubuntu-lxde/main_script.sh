df3de2c17c3a587a2892054f02e9cbb6f8919c2a
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2024-12-18 10:07:28 +0700

    ubuntu-lxde: prepare ubuntu base
    Changes include:
    	- Add script to prepare ubuntu 22.04 base.
    	- Call function ubuntu_base_prepare in main script.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- ubuntu-lxde/main_script.sh
+++ ubuntu-lxde/main_script.sh
@@ -3,10 +3,15 @@
 # Main function 
 # --------------------------------------------------------------------------------#
 
+source include/01_prepare_ubuntu_base.sh
 
 # main function
 function main(){
-
+    ubuntu_base_prepare
+    if [[ $? -eq 1 ]]; then
+        echo "ubuntu_base_prepare failed."
+        exit 1
+    fi
 }
 
 # call main
