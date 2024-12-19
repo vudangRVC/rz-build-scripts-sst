9ab4f83bc0454b404630942044a830e491cd8758
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2024-12-19 16:00:30 +0700

    ubuntu-lxde: Update Audio Video Wifi bluetooth firmware
    Change include:
    	- Update to install audio video wifi bluetooth firmware withdow show dialog.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- ubuntu-lxde/script/apt_audio_video.sh
+++ ubuntu-lxde/script/apt_audio_video.sh
@@ -5,11 +5,19 @@ export LC_ALL=C
 chmod 777 /tmp
 apt update
 
+# default config debian
+apt install -y debconf-utils
+
+# time zone data
+echo 'tzdata tzdata/Areas select Asia' | sudo debconf-set-selections
+echo 'tzdata tzdata/Zones/Asia select Ho_Chi_Minh' | sudo debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure tzdata
+
 # video for linux utils
-apt-get install v4l-utils -y
+DEBIAN_FRONTEND=noninteractive apt install v4l-utils -y
 
 # video codec:
-apt install -y ubuntu-restricted-extras
+DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-restricted-extras
 
 # Audio app
 apt install audacity -y
