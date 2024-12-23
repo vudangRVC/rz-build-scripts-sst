24c42eee17ca056c3879d74ba307fc07407d7550
Long Trinh <long.trinh.rh@bp.renesas.com>
2024-12-23 09:37:22 +0000

    ubuntu-headless-qt : Remove Audio Video, add Wifi bluetooth firmware

--- ubuntu-headless-qt/script/apt_wifi_ble.sh
+++ ubuntu-headless-qt/script/apt_wifi_ble.sh
@@ -5,9 +5,20 @@ export LC_ALL=C
 chmod 777 /tmp
 apt update
 
+# Set DEBIAN_FRONTEND globally
+export DEBIAN_FRONTEND=noninteractive
+
+# Install debconf-utils for preconfiguring
+apt install -y debconf-utils
+
+# time zone data
+echo 'tzdata tzdata/Areas select Asia' | sudo debconf-set-selections
+echo 'tzdata tzdata/Zones/Asia select Ho_Chi_Minh' | sudo debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure tzdata
+
 # install bluetooth firmware and app
-apt install linux-firmware -y
-apt install dbus-x11 -y 
-apt install apt-utils -y 
+DEBIAN_FRONTEND=noninteractive apt install linux-firmware -y
+DEBIAN_FRONTEND=noninteractive apt install dbus-x11 -y
+DEBIAN_FRONTEND=noninteractive apt install apt-utils -y
+DEBIAN_FRONTEND=noninteractive apt install libssl-dev -y
 apt install blueman -y
-apt install libssl-dev -y
