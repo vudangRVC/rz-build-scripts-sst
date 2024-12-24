f038aac958dd05662b6256e2c6228cb40ea840e9
Long Trinh <long.trinh.rh@bp.renesas.com>
2024-12-24 06:44:11 +0000

    ubuntu-headless-qt : Update missing packages, add tar.gz packaging; remove unused apt_wifi_ble script call

--- ubuntu-headless-qt/main_script.sh
+++ ubuntu-headless-qt/main_script.sh
@@ -55,12 +55,6 @@ function main(){
         exit 1
     fi
 
-    chroot_run_1_script "apt_wifi_ble.sh"
-    if [[ $? -eq 1 ]]; then
-        echo "apt_wifi_ble failed."
-        exit 1
-    fi
-
     package_rootfs
     if [[ $? -eq 1 ]]; then
         echo "package_rootfs failed."
