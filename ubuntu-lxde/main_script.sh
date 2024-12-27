ab01c0564f64deda32f5498fb7ef215dd31e4030
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2024-12-27 08:22:03 +0700

    ubuntu-lxde: add script to set permission user root and rzpi
    Changes include:
    	- Add script to set permission user root and rzpi.
    	- Add comment in main function.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- ubuntu-lxde/main_script.sh
+++ ubuntu-lxde/main_script.sh
@@ -9,63 +9,79 @@ source include/02_prepare_rootfs_qt.sh
 source include/03_prepare_conf.sh
 source include/04_mount.sh
 
-
 # main function
 function main(){
+    # prepare ubuntu base
     ubuntu_base_prepare
     if [[ $? -eq 1 ]]; then
         echo "ubuntu_base_prepare failed."
         exit 1
     fi
 
+    # prepare rootfs qt
     rootfs_qt
     if [[ $? -eq 1 ]]; then
         echo "rootfs_qt failed."
         exit 1
     fi
 
+    # prepare conf
     set_config
     if [[ $? -eq 1 ]]; then
         echo "set_config failed."
         exit 1
     fi
 
+    # mount chroot to install basic package
     chroot_run_1_script "apt_install_base.sh"
     if [[ $? -eq 1 ]]; then
         echo "set_config failed."
         exit 1
     fi
-
-    chroot_run_1_script "set_root_password.sh"
-    if [[ $? -eq 1 ]]; then
-        echo "set_root_password failed."
-        exit 1
-    fi
-
-    chroot_run_1_script "create_rzpi_user.sh"
-    if [[ $? -eq 1 ]]; then
-        echo "create_rzpi_user failed."
-        exit 1
-    fi
-
+    
+    # install audio and video package
     chroot_run_1_script "apt_audio_video.sh"
     if [[ $? -eq 1 ]]; then
         echo "apt_audio_video failed."
         exit 1
     fi
 
+    # install wifi and ble package
     chroot_run_1_script "apt_wifi_ble.sh"
     if [[ $? -eq 1 ]]; then
         echo "apt_wifi_ble failed."
         exit 1
     fi
 
+    # install lxde desktop
     chroot_run_1_script "apt_lxde_desktop.sh"
     if [[ $? -eq 1 ]]; then
         echo "apt_lxde_desktop failed."
         exit 1
     fi
 
+    # create rzpi user - normal user
+    chroot_run_1_script "create_rzpi_user.sh"
+    if [[ $? -eq 1 ]]; then
+        echo "create_rzpi_user failed."
+        exit 1
+    fi
+
+    # set root password
+    chroot_run_1_script "set_root_password.sh"
+    if [[ $? -eq 1 ]]; then
+        echo "set_root_password failed."
+        exit 1
+    fi
+
+    # set permissions
+    chroot_run_1_script "setup-set-permissions.sh"
+    if [[ $? -eq 1 ]]; then
+        echo "setup-set-permissions.sh failed."
+        exit 1
+    fi
+
+    # package rootfs to tar file
     package_rootfs
     if [[ $? -eq 1 ]]; then
         echo "package_rootfs failed."
