9947347507ada58f2c87f40c8b9226f992556462
Long Trinh <long.trinh.rh@bp.renesas.com>
2025-03-03 15:00:01 +0700

    ubuntu: now bring back completed ubuntu build script from dunfell here

---
+++ ubuntu/include/common/yocto_working.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# --------------------------------------------------------------------------#
+# This script provides functions that help main script handle yocto part
+# --------------------------------------------------------------------------#
+
+# This function help main script can build yocto
+build_yocto() {
+    # Initialize a variable to store the result
+    result=""
+
+    # Loop until we find the output file
+    while [ -z "$result" ]; do
+        # Run bitbake
+        su -c "(cd ../rz-sbc/ && IMAGE=renesas-ubuntu DISTRO=ubuntu-tiny ./rzsbc_yocto.sh build)" "$MAIN_USER"
+
+        # Check the output
+        result=$(find ../rz-sbc/yocto_rzsbc_board/build/tmp/deploy/ -name '*.tar.bz2' -exec cp {} ./core-image-qt-rzpi.tar.bz2 \; && echo "File copied successfully.")
+
+        # Exit if yocto does not build successfully
+        if [ -z "$result" ]; then
+            echo "[Yocto]: No output files found. Retrying..."
+        else
+            echo "Yocto output have been built."
+        fi
+    done
+}
+
+# This function help main script bring wic file to yocto output's directory
+move_ubuntu_to_yocto_output(){
+    # Check output folder availability
+    DIR="../rz-sbc/yocto_rzsbc_board/build/tmp/deploy/images/rzpi/target/images"
+    if [ -d "$DIR" ]; then
+        echo "Found output yocto folder"
+        mv "$OUTPUT_WIC"* $DIR
+    fi
+
+    DIR_ROOTFS="../rz-sbc/yocto_rzsbc_board/build/tmp/deploy/images/rzpi/target/images/rootfs"
+    if [ -d "$DIR_ROOTFS" ]; then
+        echo "Found output yocto rootfs folder"
+        mv "$OUTPUT_ROOTFS"* $DIR_ROOTFS
+    fi
+}
